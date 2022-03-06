dafb6db83adfeeac9603b2d6f34f9dad7e8c0b61
erfederuiz <federicoruiz.ruiz@gmail.com>
2022-03-06 14:59:33 +0100

    Using boto3 to store pickle

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,32 @@
 from flask import Flask
 import pickle
 import json
+import boto3
 
-modelo = {}
 app = Flask(__name__)
 
 def load_model():
-    with open('./models/finished_model_arima.model', "rb") as archivo_entrada:
-        modelo_arima = pickle.load(archivo_entrada)	
+    # Creating the low level functional client
+# Creating the high level object oriented interface
+    resource = boto3.resource(
+        's3',
+        aws_access_key_id = 'AKIAQJD4VX47OGV4PW5V',
+        aws_secret_access_key = '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',
+        region_name = 'eu-west-3'
+    )
+
+    with open('local_model.pkl', 'wb') as data:
+        resource.Bucket("dsftnov21-prod-test03").download_fileobj("finished_model_arima.model", data)  
+
+    with open('local_model.pkl', 'rb') as data:
+        modelo_arima = pickle.load(data)
+
+    """with open('./models/finished_model_arima.model', "rb") as archivo_entrada:
+        modelo_arima = pickle.load(archivo_entrada)	"""
+
     return modelo_arima
+
+modelo = load_model()
 
 @app.route('/')
 def hello_world():
@@ -22,6 +40,5 @@ def predict():
     return app_json
 
 if __name__ == "__main__":
-    modelo = load_model()
     app.run()
 
