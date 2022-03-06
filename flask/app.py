dcd5e33c6dcde69148f849d5215de50c9cba6e0b
erfederuiz <federicoruiz.ruiz@gmail.com>
2022-03-06 13:24:58 +0100

    Endpoint message

--- flask/app.py
+++ flask/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World Pickle loaded!\n\n'
+	return 'Endpoint Pickle loaded!\n\n'
 
 if __name__ == "__main__":
     with open('./models/finished_model_arima.model', "rb") as archivo_entrada:
