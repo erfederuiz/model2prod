457f4978351729d9d2e107ad721934b74591b9b2
erfederuiz <federicoruiz.ruiz@gmail.com>
2022-03-06 18:44:33 +0100

    Using logs

--- flask/app.py
+++ flask/app.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template, request
 import pickle
 import json
 import boto3
+import logging
 
 app = Flask(__name__)
+gunicorn_error_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers.extend(gunicorn_error_logger.handlers)
+app.logger.setLevel(logging.INFO)
 
 def load_model(aws_info):
     # Creating the low level functional client
 # Creating the high level object oriented interface
+    app.logger.info('Loading file ' + aws_info['model_file'] + ' from S3')
     resource = boto3.resource(
         's3',
         aws_access_key_id = aws_info["key"] ,
@@ -18,10 +23,11 @@ def load_model(aws_info):
     with open('local_model.pkl', 'wb') as data:
         resource.Bucket(aws_info['s3_bucket']).download_fileobj(aws_info['model_file'], data)  
 
+    app.logger.info('Unpickling file ' + aws_info['model_file'] + ' to local_model.pkl')
     with open('local_model.pkl', 'rb') as data:
         modelo_arima = pickle.load(data)
 
-
+    app.logger.info('Model ready')
     return modelo_arima
 
 def get_aws_creds():
