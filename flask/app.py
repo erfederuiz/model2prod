62e2c13400be84627e0acbcb7cae1236cb82232a
erfederuiz <federicoruiz.ruiz@gmail.com>
2022-03-06 18:22:37 +0100

    Parametrize aws info in aws_info.txt

--- flask/app.py
+++ flask/app.py
@@ -5,30 +5,28 @@ import boto3
 
 app = Flask(__name__)
 
-def load_model(creds):
+def load_model(aws_info):
     # Creating the low level functional client
 # Creating the high level object oriented interface
     resource = boto3.resource(
         's3',
-        aws_access_key_id = creds["key"] ,
-        aws_secret_access_key = creds["secret"] ,
-        region_name = 'eu-west-3'
+        aws_access_key_id = aws_info["key"] ,
+        aws_secret_access_key = aws_info["secret"] ,
+        region_name = aws_info['aws_region']
     )
 
     with open('local_model.pkl', 'wb') as data:
-        resource.Bucket("dsftnov21-prod-test03").download_fileobj("finished_model_arima.model", data)  
+        resource.Bucket(aws_info['s3_bucket']).download_fileobj(aws_info['model_file'], data)  
 
     with open('local_model.pkl', 'rb') as data:
         modelo_arima = pickle.load(data)
 
-    """with open('./models/finished_model_arima.model', "rb") as archivo_entrada:
-        modelo_arima = pickle.load(archivo_entrada)	"""
 
     return modelo_arima
 
 def get_aws_creds():
     myvars = {}
-    with open("aws_creds.txt") as myfile:
+    with open("aws_info.txt") as myfile:
         for line in myfile:
             name, var = line.partition("=")[::2]
             myvars[name.strip()] = var.rstrip()
@@ -41,13 +39,10 @@ modelo = load_model(get_aws_creds())
 def hello_world():
 	return 'Endpoint Pickle loaded!\n\n'
 
-@app.route('/form')
-def form():
-    return render_template('form.html')
 
-@app.route('/predict')
-def predict():
-    predictions = modelo.predict(10)
+@app.route('/predict/<int:periods>')
+def predict(periods):
+    predictions = modelo.predict(periods)
     result = dict(enumerate(predictions.flatten(), 1))
     app_json = json.dumps(result)
     return app_json
